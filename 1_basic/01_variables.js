cc2fb4bce3a57d0d31c89e65e80eef488479dc99
vandita731 <vanditaj008@gmail.com>
2025-05-21 17:29:45 +0000

    data type

---
+++ 1_basic/01_variables.js
@@ -0,0 +1,18 @@
+const account_Id = 144356
+let accountEmail = "vandita123@gmail.com"
+var accountPassward = "12345"
+accountCity = "indore"
+let accountState;
+
+// account_Id=253535 this we can not write because we used const
+
+console.table([accountEmail,accountPassward,account_Id])
+
+accountEmail="vandit"
+accountPassward="345463"
+accountCity="bhopal"
+console.table([accountEmail,accountPassward,account_Id,accountState])
+/*we perfer to use let than var 
+because it is a old
+way to declare the variable 
+also the issue   */
---
+++ 1_basic/02_data_type.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+let name = "vandita"
+let state;
+
+/* number => 
+   big int
+   string => ""
+   boolean => true/flase
+   null => standalone value
+   symbol => unique
+   object
+   */
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
