0633b0488fc88bc61e4fd932f8e4cb4a229d15be
vandita731 <vanditaj008@gmail.com>
2025-05-22 17:49:31 +0000

    opertaions

--- 1_basic/04_convertionOperation.js
+++ 1_basic/04_convertionOperation.js
@@ -32,3 +32,44 @@ let age = 23
 let stringInNumber= String(age)
 console.log(stringInNumber)
 console.log(typeof stringInNumber)
+
+
+// *******************operation*******************
+
+console.log("*****************operatins******************")
+
+let value = 3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-1)
+console.log(2*4) // multiplication
+console.log(4/2)
+console.log(2**3) // power 
+console.log(2%3) // remainder
+
+
+let str1 = "hello "
+let str2 = "vandita"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1"+2+3) // first string then ever thing is converted into string
+console.log(1+2+"3") // first and second number both are number so they add then a string is add to it
+console.log(1+"2"+3)
+
+console.log(+true) // convert this boolean into number 
+console.log(+"") // convert this string into 
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter++)
+console.log(++gameCounter)
+
+
+
+
+
