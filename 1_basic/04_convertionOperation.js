93b028c3a6a929a1095377e10badbd79f2b6a600
vandita731 <vanditaj008@gmail.com>
2025-05-22 17:05:40 +0000

    convertion of data type

---
+++ 1_basic/04_convertionOperation.js
@@ -0,0 +1,34 @@
+let score="33abc"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+console.log("----------")
+
+/* 
+"33" = 33
+"33abc" = NaN(not a number)
+true = 1 ; false = 0;
+
+*/
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(booleanLoggedIn)
+
+console.log("----------")
+
+
+// 1 = true 
+// 0 = false
+// "" = false
+// "asfdg" = true
+
+let age = 23
+let stringInNumber= String(age)
+console.log(stringInNumber)
+console.log(typeof stringInNumber)
