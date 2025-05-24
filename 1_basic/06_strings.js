81890939b3859c4484bc12ca1191c9c98931024f
vandita731 <vanditaj008@gmail.com>
2025-05-24 19:10:02 +0000

    strings

---
+++ 1_basic/06_strings.js
@@ -0,0 +1,28 @@
+const name = "vandita"
+const repocount = 1
+console.log(name+repocount+" value");
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+const gameName= new String("misty-jain")
+
+console.log(gameName[1])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "      vandita     "
+
+console.log(newStringOne)
+console.log(newStringOne.trim())
