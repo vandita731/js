2b66359eb95c6d0a23aa92c40c2063b913ee0018
vandita731 <vanditaj008@gmail.com>
2025-05-25 11:10:39 +0000

    dates and time

---
+++ 1_basic/08_dates_and_time.js
@@ -0,0 +1,13 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleTimeString())
+
+console.log(typeof myDate)
