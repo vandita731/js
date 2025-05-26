ddad013f7c92beb671e5a29baede3e3cba8122d7
vandita731 <vanditaj008@gmail.com>
2025-05-26 14:26:03 +0000

    date and time methods

--- 1_basic/08_dates_and_time.js
+++ 1_basic/08_dates_and_time.js
@@ -11,3 +11,27 @@ console.log(myDate.toLocaleString())
 console.log(myDate.toLocaleTimeString())
 
 console.log(typeof myDate)
+
+let myCreateDate = new Date(2025,0,7)
+console.log(myCreateDate.toDateString())
+
+let newDate = new Date("2025-01-26")
+console.log(newDate.toLocaleString())
+
+let myTimeStamp = Date.now() // to get time stamp in milli second  
+
+
+console.log(myTimeStamp);
+
+console.log(myCreateDate.getTime())
+console.log(Math.floor(Date.now()/1000)) // milli second to second
+
+let newDate_1 = new Date()
+console.log(newDate_1.getDay())
+
+console.log(newDate_1.toLocaleString('default',{
+    //weekday:"long",
+    timeStyle:"full"
+
+}))
+
